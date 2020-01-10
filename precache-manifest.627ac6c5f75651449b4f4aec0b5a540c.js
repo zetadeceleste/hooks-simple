63c49e77bff4ccfd72d65c3e0b6949db4bb25e1a
Celeste <czapata@vaypol.com.ar>
2020-01-10 13:24:15 -0300

    Init commit

---
+++ precache-manifest.627ac6c5f75651449b4f4aec0b5a540c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2e500c1ffb518cb6c268eb8fc2c9af",
+    "url": "/hooks-simple/index.html"
+  },
+  {
+    "revision": "48ae17b13aa7b80dc9de",
+    "url": "/hooks-simple/static/js/2.82018205.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hooks-simple/static/js/2.82018205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e48ed655b35daba26ae7",
+    "url": "/hooks-simple/static/js/main.af8bf216.chunk.js"
+  },
+  {
+    "revision": "b21f0f6cf4db18209a21",
+    "url": "/hooks-simple/static/js/runtime-main.1ab429b0.js"
+  }
+]);
